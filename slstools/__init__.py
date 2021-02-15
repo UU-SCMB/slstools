d1f824f0391a336144dee118689fb1fb327c6efb
Roy Hoitink <L.D.Hoitink@uu.nl>
2021-02-15 12:48:28 +0100

    Removed unused sls.py file from __init__.py

--- slstools/__init__.py
+++ slstools/__init__.py
@@ -1,6 +1,5 @@
-from .sls import SLS
 from .experiment import Experiment
 from .theory import Miescat
 from .model import Model
 
-__all__ = ["SLS", "Experiment", "Miescat", "Model"]
+__all__ = ["Experiment", "Miescat", "Model"]
